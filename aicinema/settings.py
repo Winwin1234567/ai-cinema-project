8e9a0ca5b016d87cf7d9f0d47ae563b35589f75c
Winwin1234567 <jamebond2060@gmail.com>
2025-09-01 01:08:57 +0630

    Configure static and media settings for Railway

--- aicinema/settings.py
+++ aicinema/settings.py
@@ -124,7 +124,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'  # Path object
 STATICFILES_DIRS = [BASE_DIR / 'static'] # List of Path objects
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
