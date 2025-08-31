53d15aa463ac5a44b59472d3c9120939bc67cd12
Winwin1234567 <jamebond2060@gmail.com>
2025-09-01 02:04:58 +0630

    Update settings

--- aicinema/settings.py
+++ aicinema/settings.py
@@ -126,7 +126,8 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'  # Path object
 STATICFILES_DIRS = [BASE_DIR / 'static'] # List of Path objects
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
-
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
